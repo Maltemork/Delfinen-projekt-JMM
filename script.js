83eda357d968a549554f0853e2a8e0234788b464
Maltemork <maltems1996@gmail.com>
2023-05-10 10:54:04 +0200

    first prototype of get data
    fills out 2 arrays from the endpoint

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 
 window.addEventListener("load", start);
 
-function start() {
+const endpoint = "https://delfinen-4077b-default-rtdb.europe-west1.firebasedatabase.app/";
+
+const members = [];
+const users = [];
+
+async function start() {
+  getData();
   console.log("App is running! 😊");
 }
+
+async function getData() {
+  console.log("Getting data!");
+  
+  const responseUsers = await fetch(`${endpoint}/users.json`)
+  const dataUsers = await responseUsers.json();
+
+  const responseMembers = await fetch(`${endpoint}/members.json`);
+  const dataMembers = await responseMembers.json();
+
+  users.push(dataUsers);
+  members.push(dataMembers);
+}
+
