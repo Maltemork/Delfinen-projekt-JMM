fcc72c56f7309c927b6bda706a000a46eaec915b
Malte <maltebjoerklund@gmail.com>
2023-05-11 17:52:30 +0200

    Update member (ikke fungerende)

--- script.js
+++ script.js
@@ -106,125 +106,193 @@ function createClicked() {
   document
     .querySelector("#create-form")
     .addEventListener("submit", createMember);
-}
-
-function createMember(event) {
-  event.preventDefault();
-  document
-    .querySelector("#create-form")
-    .removeEventListener("submit", createMember);
 
   const createForm = document.querySelector("#create-form");
+  createForm.reset();
 
-  const newMember = {
-    activity: createForm.activity.value,
-    age: createForm.age.value,
-    arrears: 0,
-    competition: checkCompetition(),
-    disciplines: chosenDisciplines(),
-    email: createForm.email.value,
-    group: correctGroup(),
-    name: createForm.name.value,
-    phone: createForm.phone.value,
-    subscription: correctSubscription(),
-    type: createForm.type.value,
-  };
+  function createMember(event) {
+    event.preventDefault();
+    document
+      .querySelector("#create-form")
+      .removeEventListener("submit", createMember);
 
-  createdMemberSend(newMember);
-  document.querySelector("#create-form").reset();
-  document.querySelector("#create-dialog").close();
+    const newMember = {
+      activity: createForm.activity.value,
+      age: createForm.age.value,
+      arrears: 0,
+      competition: checkCompetition(createForm.type.value),
+      disciplines: chosenDisciplines(createForm.type.value, "create"),
+      email: createForm.email.value,
+      group: correctGroup(createForm.age.value, createForm.type.value),
+      name: createForm.name.value,
+      phone: createForm.phone.value,
+      subscription: correctSubscription(
+        createForm.age.value,
+        createForm.activity.value
+      ),
+      type: createForm.type.value,
+    };
 
-  function correctSubscription() {
-    let subscription = 0;
-    if (createForm.age.value < 18) {
-      subscription = 1000;
-    } else if (createForm.age.value <= 60) {
-      subscription = 1600;
-    } else {
-      subscription = 1200;
-    }
-    if (createForm.activity.value == "passive") {
-      subscription = 500;
-    }
-    return subscription;
-  }
+    createdMemberSend(newMember);
 
-  function correctGroup() {
-    let group = 0;
-    if (createForm.type.value == "comp") {
-      if (createForm.age.value < 18) {
-        group = 1;
+    document.querySelector("#create-dialog").close();
+    async function createdMemberSend(newMember) {
+      console.log("Posting member");
+
+      const jsonString = JSON.stringify(newMember);
+
+      const response = await fetch(`${endpoint}/members.json`, {
+        method: "POST",
+        body: jsonString,
+      });
+
+      if (response.ok) {
+        console.log("Member Creation Successful");
+        location.reload();
       } else {
-        group = 2;
+        console.log("Error during posting");
       }
-    }
-    return group;
-  }
-
-  function checkCompetition() {
-    let comp = {};
-    if (createForm.type.value == "comp") {
-      comp = { lokation: "", meet: "", time: "" };
-    }
-    return comp;
-  }
-
-  function chosenDisciplines() {
-    console.log("Chosen Disciplines");
-    const disciplines = {};
-
-    if (createForm.type.value == "comp") {
-      const inputs = createForm.querySelectorAll("input[type='checkbox']");
-      const selected = [];
-
-      for (const input of inputs) {
-        if (input.checked) {
-          selected.push(input.value);
-        }
-      }
-
-      for (const discipline of selected) {
-        switch (discipline) {
-          case "backcrawl":
-            disciplines.backcrawl = { date: [""], time: [0] };
-            break;
-          case "butterfly":
-            disciplines.butterfly = { date: [""], time: [0] };
-            break;
-          case "chest":
-            disciplines.chest = { date: [""], time: [0] };
-            break;
-          case "crawl":
-            disciplines.crawl = { date: [""], time: [0] };
-            break;
-        }
-      }
-    }
-    return disciplines;
-  }
-
-  async function createdMemberSend(newMember) {
-    console.log("Posting member");
-
-    const jsonString = JSON.stringify(newMember);
-
-    const response = await fetch(`${endpoint}/members.json`, {
-      method: "POST",
-      body: jsonString,
-    });
-
-    if (response.ok) {
-      console.log("Member Creation Successful");
-      location.reload();
-    } else {
-      console.log("Error during posting");
     }
   }
 }
 
 function editMemberClicked(member) {
   console.log(`Editing ${member.name}`);
+  document
+    .querySelector(`#edit-${member.id}`)
+    .removeEventListener("click", () => {
+      editMemberClicked(member);
+    });
+
+  const updateForm = document.querySelector("#update-form");
+  updateForm.activity.value = member.activity;
+  updateForm.age.value = member.age;
+  updateForm.email.value = member.email;
+  updateForm.name.value = member.name;
+  updateForm.phone.value = member.phone;
+  updateForm.type.value = member.type;
+
+  const dialog = document.querySelector("#update-dialog");
+  dialog.showModal();
+
+  document
+    .querySelector("#update-form")
+    .addEventListener("submit", updateMember);
+
   function updateMember() {
-    function sendUpdatedMember() {}
+    console.log("Update member");
+    document
+      .querySelector("#update-form")
+      .removeEventListener("submit", updateMember);
+
+    const updatedMember = {
+      activity: updateForm.activity.value,
+      age: updateForm.age.value,
+      arrears: member.arrears,
+      competition: checkCompetition(updateForm.type.value),
+      disciplines: chosenDisciplines(updateForm.type.value, "update"),
+      email: updateForm.email.value,
+      group: correctGroup(updateForm.age.value, updateForm.type.value),
+      name: updateForm.name.value,
+      phone: updateForm.phone.value,
+      subscription: correctSubscription(
+        updateForm.age.value,
+        updateForm.activity.value
+      ),
+      type: updateForm.type.value,
+    };
+    sendUpdatedMember(member.id, updatedMember);
+    dialog.close();
+
+    async function sendUpdatedMember(memberId, updatedMember) {
+      console.log("Updating member");
+      console.log(memberId);
+      console.log(updatedMember);
+
+      // const jsonString = JSON.stringify(updatedMember);
+
+      // const response = await fetch(`${endpoint}/members/${memberId}.json`, {
+      //   method: "PUT",
+      //   body: jsonString,
+      // });
+
+      // if (response.ok) {
+      //   console.log("Update successfull");
+      //   location.reload();
+      // } else {
+      //   console.log("Failed to update");
+      // }
+    }
   }
+}
+
+function correctSubscription(age, activity) {
+  let subscription = 0;
+  if (age < 18) {
+    subscription = 1000;
+  } else if (age <= 60) {
+    subscription = 1600;
+  } else {
+    subscription = 1200;
+  }
+  if (activity == "passive") {
+    subscription = 500;
+  }
+  return subscription;
+}
+
+function correctGroup(age, type) {
+  let group = 0;
+  if (type == "comp") {
+    if (age < 18) {
+      group = 1;
+    } else {
+      group = 2;
+    }
+  }
+  return group;
+}
+
+function checkCompetition(type) {
+  let comp = {};
+  if (type == "comp") {
+    comp = { lokation: "", meet: "", time: "" };
+  }
+  return comp;
+}
+
+function chosenDisciplines(type, action) {
+  console.log("Chosen Disciplines");
+  const disciplines = {};
+
+  if (type == "comp") {
+    const inputs = document
+      .querySelector(`#${action}-form`)
+      .querySelectorAll("input[type='checkbox']");
+    const selected = [];
+
+    for (const input of inputs) {
+      if (input.checked) {
+        selected.push(input.value);
+      }
+    }
+
+    for (const discipline of selected) {
+      switch (discipline) {
+        case "backcrawl":
+          disciplines.backcrawl = { date: [""], time: [0] };
+          break;
+        case "butterfly":
+          disciplines.butterfly = { date: [""], time: [0] };
+          break;
+        case "chest":
+          disciplines.chest = { date: [""], time: [0] };
+          break;
+        case "crawl":
+          disciplines.crawl = { date: [""], time: [0] };
+          break;
+      }
+    }
+  }
+  return disciplines;
 }
