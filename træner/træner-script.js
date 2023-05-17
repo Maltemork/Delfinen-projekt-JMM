e4845aeed9edc618c7e620ea7711a8377e1488b9
Maltemork <maltems1996@gmail.com>
2023-05-17 11:23:51 +0200

    træner tabel done
    træner tabel færdiggjort hvor søg og sort stadig virker.

--- træner/træner-script.js
+++ træner/træner-script.js
@@ -4,25 +4,49 @@ import { getData, members } from "../crud.js";
 
 window.addEventListener("load", start);
 
+let filteredTeamsArray = [];
+
 async function start() {
   await getData();
-  members.forEach(showMembersTable);
-  
-  sortTable();
+  changeTeamTable();
   document.querySelector("#sort-dropdown").addEventListener("change", sortTable);
   document.querySelector("#go-to-login").addEventListener("click", () => 
   {
     window.location.href = "../login/login.html"
   });
   document.querySelector("#search-members").addEventListener("input", searchMembers);
+  document.querySelector("#hold-selector").addEventListener("change", changeTeamTable);
 }
 
+function changeTeamTable() {
+  filteredTeamsArray = [];
+  if (document.querySelector("#hold-selector").value == "hold-junior"){
+    console.log("JUNIOR GROUP SELECTED");
+    clearTable();
+    for (let i = 0; i < members.length; i++){
+        if (members[i].group == 1) {
+        filteredTeamsArray.push(members[i]);
+        }
+      }
+  } else if (document.querySelector("#hold-selector").value == "hold-senior"){
+    console.log("SENIOR GROUP SELECTED");
+    clearTable();
+    for (let i = 0; i < members.length; i++){
+        if (members[i].group == 2) {
+        filteredTeamsArray.push(members[i]);
+        }
+      }
+  }
+  filteredTeamsArray.forEach(showMembersTable);
+}
+
+
 function showMembersTable(member) {
-  
   document.querySelector("#members-table").insertAdjacentHTML(
     "beforeend",
     /* html */ `
         <tr class="table-item">
+          <td>${member.group}</td>
           <td>${isActiveSymbol(member.activity)}</td>
           <td>${member.name}</td>
           <td>${member.age}</td>
@@ -42,23 +66,13 @@ function isActiveSymbol(activity) {
 }
 
 function sortTable() {
-
-  document.querySelector("#members-table").innerHTML = /*HTML*/
-  `
-  <tr class="table-item">
-      <td>Aktiv</td>
-      <td>Navn</td>
-      <td>Alder</td>
-      <td>E-mail</td>
-      <td>Telefon</td>
-  </tr>`;
-
+  clearTable();
   const sortOption = document.querySelector("#sort-dropdown").value;
 
   // læser værdien i dropdown menuen og sorterer efter valgt.
   if (sortOption == "name-AZ") {
     // Sorter efter navn A-Z.
-    const name_AZ = members.sort((a,b) => {
+    const name_AZ = filteredTeamsArray.sort((a,b) => {
       let nameA = a.name.toLowerCase();
       let nameB = b.name.toLowerCase();
   
@@ -71,7 +85,7 @@ function sortTable() {
   }
   if (sortOption == "name-ZA") {
     //Sorter efter navn Z-A
-    const name_ZA = members.sort((a,b) => {
+    const name_ZA = filteredTeamsArray.sort((a,b) => {
       let nameA = a.name.toLowerCase();
       let nameB = b.name.toLowerCase();
   
@@ -83,7 +97,7 @@ function sortTable() {
   
   }
   if (sortOption == "age-LOW") {
-    const age_LOW = members.sort((a,b) => {
+    const age_LOW = filteredTeamsArray.sort((a,b) => {
       if (a.age < b.age) return -1;
       return 1;
     });
@@ -91,7 +105,7 @@ function sortTable() {
 
   }
   if (sortOption == "age-HIGH") {
-    const age_HIGH = members.sort((a,b) => {
+    const age_HIGH = filteredTeamsArray.sort((a,b) => {
       if (a.age > b.age) return -1;
       return 1;
     });
@@ -100,7 +114,7 @@ function sortTable() {
   }
 
   if (sortOption == "active-YES") {
-    const active_YES = members.sort((a,b) => {
+    const active_YES = filteredTeamsArray.sort((a,b) => {
       if (a.activity < b.activity) return -1;
       return 1;
     });
@@ -108,7 +122,7 @@ function sortTable() {
     
   }
   if (sortOption == "active-NO") {
-    const active_NO = members.sort((a,b) => {
+    const active_NO = filteredTeamsArray.sort((a,b) => {
       if (a.activity > b.activity) return -1;
       return 1;
     });
@@ -119,23 +133,29 @@ function sortTable() {
 
 /* ----- Seach - Member -----*/
 function searchMembers() {
-  document.querySelector("#members-table").innerHTML = `
-  <tr class="table-item">
-  <td>Aktiv</td>
-  <td>Navn</td>
-  <td>Alder</td>
-  <td>E-mail</td>
-  <td>Telefon</td>
-</tr>
-  `;
+  clearTable();
   const searchInput = document.querySelector("#search-members").value.toLowerCase();
   //filter members based on search input, without being case sensitive.
-  const filteredMembers = members.filter((member) =>
+  const filteredMembers = filteredTeamsArray.filter((member) =>
     member.name.toLowerCase().includes(searchInput)
   );
   if (searchInput.length !== 0) {
     filteredMembers.forEach(showMembersTable);
   } else {
-    members.forEach(showMembersTable)
+    filteredTeamsArray.forEach(showMembersTable)
   }
+}
+
+function clearTable() {
+  document.querySelector("#members-table").innerHTML = /*HTML*/
+  `
+  <tr class="table-item">
+      <td>Hold</td>
+      <td>Aktiv</td>
+      <td>Navn</td>
+      <td>Alder</td>
+      <td>E-mail</td>
+      <td>Telefon</td>
+  </tr>`;
+
 }
